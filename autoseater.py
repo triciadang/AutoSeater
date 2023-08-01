c6335f1b72313e1a17450ec729f08b2334dfd515
triciadang <tricia.dang3@gmail.com>
2023-07-31 20:50:44 -0500

    COMPLETED - seats everybody
    Only thing left is to remove unnecessary registrants

--- autoseater.py
+++ autoseater.py
@@ -20,63 +20,78 @@ def what_table_number(number_in_group):
     return table_number
 
 def main():
+
     #import csv
-
-
-
     with open("C:\\Users\\trici\\Downloads\\registrants (18).csv",'r') as csv_file:
     #with open('C:\\Users\\taduser\\Documents\\EventBrite\\registrants.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
 
+        #opens separate csv to write Seating Tables to
         with open("C:\\Users\\trici\\Downloads\\seatingChart.csv",'w',newline="") as seating_file:
         #with open('C:\\Users\\taduser\\Documents\\EventBrite\\seatingChart.csv','w') as seating_file:
             writer = csv.writer(seating_file)
 
-            table_num = 1
+            #Initiates values
             singular_group = []
             number_in_group = 0
 
             for row in reader:
+                #For header row
                 if reader.line_num == 1:
                     row.append("Table Number")
                     writer.writerow(row)
+
+                #For first real row of data
                 elif reader.line_num == 2:
                     singular_group.append(row)
                     number_in_group += 1
+
+                #If registrants registered together
                 elif prev_row[0] == row[0]:
                     singular_group.append(row)
                     number_in_group += 1
+
+                #If we are on a new group
                 else:
-                    #number_in_group <= 8
-                    if number_in_group <= 8:
-                        table_num = what_table_number(number_in_group)
-                        for each in singular_group:
-                            each.append(table_num)
-                            writer.writerow(each)
-
-                    #if there are more than 8 people that registered together
-                    else:
-                        n = 0
-                        overall_number_in_group = number_in_group
-                        while number_in_group > 8:
-                            table_num = what_table_number(8)
-                            for i in range(8 * n + 0, 8 * n + 8):
-                                singular_group[i].append(table_num)
-                                writer.writerow(singular_group[i])
-                            n += 1
-                            number_in_group -= 8
-
-                        table_num = what_table_number(number_in_group)
-                        for i in range(8*n + 0, overall_number_in_group):
+                    n = 0
+                    overall_number_in_group = number_in_group
+                    while number_in_group > 8:
+                        table_num = what_table_number(8)
+                        for i in range(8 * n + 0, 8 * n + 8):
                             singular_group[i].append(table_num)
                             writer.writerow(singular_group[i])
+                        n += 1
+                        number_in_group -= 8
 
-                    #new one is only one in group
+                    # reduce until get a number <= 8
+                    table_num = what_table_number(number_in_group)
+                    for i in range(8 * n + 0, overall_number_in_group):
+                        singular_group[i].append(table_num)
+                        writer.writerow(singular_group[i])
+
+                    #new different one is only one in group
                     singular_group = []
                     singular_group.append(row)
                     number_in_group = 1
 
                 prev_row = row
+
+            #no more rows read - write final group
+            n = 0
+            overall_number_in_group = number_in_group
+            while number_in_group > 8:
+                table_num = what_table_number(8)
+                for i in range(8 * n + 0, 8 * n + 8):
+                    singular_group[i].append(table_num)
+                    writer.writerow(singular_group[i])
+                n += 1
+                number_in_group -= 8
+
+            #reduce until get a number < 8
+            table_num = what_table_number(number_in_group)
+            for i in range(8 * n + 0, overall_number_in_group):
+                singular_group[i].append(table_num)
+                writer.writerow(singular_group[i])
 
 
 if __name__ == "__main__":
