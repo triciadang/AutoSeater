69e87b00616ca5fbb0daf02a0a78902ed880fabe
triciadang <tricia.dang3@gmail.com>
2023-07-31 20:27:22 -0500

    seats everyone
    need to deal with instance there is more than eight people in the group

--- autoseater.py
+++ autoseater.py
@@ -1,41 +1,69 @@
 import csv
 
+#global variable
+SEATING_TABLE = {str(i): 8 for i in range(1, 126)}
+
+def what_table_number(number_in_group):
+    table_number = 1
+    table_found = 0
+
+    if number_in_group > 8:
+        print("NEED HELP")
+
+    while table_found == 0:
+        if SEATING_TABLE[str(table_number)] >= number_in_group:
+            table_number = str(table_number)
+            SEATING_TABLE[str(table_number)] = SEATING_TABLE[str(table_number)]-number_in_group
+            # exits while loop
+            table_found = 1
+        else:
+            table_number += 1
+
+
+    return table_number
 
 def main():
     #import csv
-    header = ['Date Completed','Register','Register ($ Amount)','Rank/Title','Name (Last Name)','Name (First Name)',
-              'Unit/Organization/Business','Sponsor or Donor?','Email','Mobile Phone Number',	'Menu Options']
-    with open('C:\\Users\\taduser\\Documents\\EventBrite\\registrants.csv', 'r') as csv_file:
+
+
+
+    with open("C:\\Users\\trici\\Downloads\\registrants (18).csv",'r') as csv_file:
+    #with open('C:\\Users\\taduser\\Documents\\EventBrite\\registrants.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
 
-        with open('C:\\Users\\taduser\\Documents\\EventBrite\\seatingChart.csv','w') as seating_file:
+        with open("C:\\Users\\trici\\Downloads\\seatingChart.csv",'w',newline="") as seating_file:
+        #with open('C:\\Users\\taduser\\Documents\\EventBrite\\seatingChart.csv','w') as seating_file:
             writer = csv.writer(seating_file)
-            writer.writerow(header)
-        table_num = 0
-        singular_group = []
-        number_in_group = 0
-        prev_row = None
 
-        for row in reader:
-            print(reader.line_num)
-            if reader.line_num == 1:
+            table_num = 1
+            singular_group = []
+            number_in_group = 0
+
+            for row in reader:
+                if reader.line_num == 1:
+                    row.append("Table Number")
+                    writer.writerow(row)
+                elif reader.line_num == 2:
+                    singular_group.append(row)
+                    number_in_group += 1
+                elif prev_row[0] == row[0]:
+                    singular_group.append(row)
+                    number_in_group += 1
+                else:
+                    table_num = what_table_number(number_in_group)
+                    for each in singular_group:
+                        print(each)
+                        each.append(table_num)
+                        writer.writerow(each)
+
+                    #need to change
+                    #table_num += 1
+                    #new one is only one in group
+                    singular_group = []
+                    singular_group.append(row)
+                    number_in_group = 1
+
                 prev_row = row
-                print("here")
-            if prev_row[2] == row[2]:
-                number_in_group += 1
-                singular_group += row
-                print("there")
-            else:
-                for each in singular_group:
-                    print(each)
-                    each = each + str(table_num)
-                    writer.writerow(each)
-                table_num += 1
-                singular_group = []
-                number_in_group = 0
-                table_num += 1
-                print("s")
-            prev_row = row
 
 
     #parse csv
